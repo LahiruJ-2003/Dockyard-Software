d01c2bec8b5868146ff0a7ca413fe7fa48ab57b6
Lahiru Rajakaruna Jayasinghe <140371042+LahiruJ-2003@users.noreply.github.com>
2025-01-29 09:03:04 +0530

    team_file
    team file

---
+++ app/Pages/team/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence, useScroll, useMotionValueEvent } from "framer-motion";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Footer from "@/components/Footer";
+import { navItems } from "@/data";
+import CoreValues from "@/app/Pages/team/CoreValues";
+
+export default function TeamPage() {
+  const { scrollYProgress } = useScroll();
+  const [logoVisible, setLogoVisible] = useState(true);
+  const heroRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const heroSection = heroRef.current;
+    if (!heroSection) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setLogoVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 1.0,
+      }
+      
+    );
+
+    observer.observe(heroSection);
+
+    return () => {
+      if (heroSection) observer.unobserve(heroSection);
+    };
+  }, []);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    if (typeof current === "number") {
+      const direction = current - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        setLogoVisible(true);
+      } else {
+        setLogoVisible(direction < 0);
+      }
+    }
+  });
+
+  return (
+    <main className="relative dark:bg-black-100 bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+      <FloatingNav navItems={navItems} />
+        <AnimatePresence mode="wait">
+          {logoVisible && (
+            <motion.header
+              initial={{ opacity: 1, y: -100 }}
+              animate={{ y: logoVisible ? 0 : -100, opacity: logoVisible ? 1 : 0 }}
+              transition={{ duration: 0.2 }}
+              className="flex items-center justify-between py-4 fixed top-10 left-0 right-0 z-50 px-4"
+            >
+              {/* Logo with animation matching the navbar */}
+              <motion.div
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0, y: -17 }}
+                transition={{ duration: 0.3 }}
+                className="text-xl font-bold pl-4"
+              >
+                
+              </motion.div>
+              
+            </motion.header>
+          )}
+        </AnimatePresence>
+
+        <CoreValues/>
+
+        
+        
+
+        {/* Footer */}
+        <Footer/>
+      </div>
+    </main>
+  );
+}
+
+
+
+
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/66f3f915e5982d6c7bb4196a/1i8kga016';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
