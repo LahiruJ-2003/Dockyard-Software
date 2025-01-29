8f260d7df68cbd752b3a3dca1144b00a0ac1dcee
Lahiru Rajakaruna Jayasinghe <140371042+LahiruJ-2003@users.noreply.github.com>
2025-01-29 15:51:32 +0530

    bg_beam_added_(line27)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
+import { BackgroundBeams } from "./ui/background-beams";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
@@ -16,11 +17,14 @@ const Hero = () => {
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
+
         <Spotlight
           className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="purple" />
+        <BackgroundBeams/>
       </div>
 
       {/**
